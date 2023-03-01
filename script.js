fc875f2cc7650b87904eefccf7d6bdeeb7409fcc
knast <knast808@gmail.com>
2023-03-01 23:45:02 +0100

    Add the layout of the website

---
+++ script.js
@@ -0,0 +1,14 @@
+let myLibrary = [];
+
+function Book(title, author, pages, hasRead) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.hasRead = hasRead;
+}
+
+function addBookToLibrary() {
+    for(let i = 0; i < myLibrary.length; i++) {
+        
+    }
+}
