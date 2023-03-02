9902d2f318aa021742986df2c77e5ddb1c5c1bba
knast <knast808@gmail.com>
2023-03-02 02:54:20 +0100

    Restructure the HTML of the project and added popup form window along with close button closing the form

--- script.js
+++ script.js
@@ -1,3 +1,17 @@
+const addBookBtn = document.querySelector('.add-book');
+const bookInfo = document.querySelector('.book-info');
+const closeBtn = document.querySelector('.close');
+
+
+addBookBtn.addEventListener('click', () => {
+    bookInfo.classList.remove('hidden');
+})
+
+closeBtn.addEventListener('click', () => {
+    bookInfo.classList.add('hidden');
+}) 
+
+
 let myLibrary = [];
 
 function Book(title, author, pages, hasRead) {
@@ -12,3 +26,4 @@ function addBookToLibrary() {
         
     }
 }
+
