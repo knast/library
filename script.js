4fa41c7046cde91880124196ee286be6aaf63e28
knast <knast808@gmail.com>
2023-03-03 23:40:59 +0100

    Add the functionality to buttons and styling to generated output

--- script.js
+++ script.js
@@ -1,29 +1,82 @@
 const addBookBtn = document.querySelector('.add-book');
 const bookInfo = document.querySelector('.book-info');
 const closeBtn = document.querySelector('.close');
+const library = document.querySelector('.library');
 
+const addToLibraryBtn = document.querySelector('.add-to-library');
 
 addBookBtn.addEventListener('click', () => {
     bookInfo.classList.remove('hidden');
+    
+    
 })
 
 closeBtn.addEventListener('click', () => {
     bookInfo.classList.add('hidden');
+    
 }) 
+
+
+
+
 
 
 let myLibrary = [];
 
-function Book(title, author, pages, hasRead) {
+function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
-    this.hasRead = hasRead;
 }
 
+let i = 0;
+
 function addBookToLibrary() {
-    for(let i = 0; i < myLibrary.length; i++) {
-        
+    const card = document.createElement('div');
+    card.classList.add('card');
+    const bookTitle = document.createElement('p');
+    const bookAuthor = document.createElement('p');
+    const bookPages = document.createElement('p');
+    const readBtn = document.createElement('button');
+    const removeBtn = document.createElement('button');
+    bookTitle.textContent = `Title: ${myLibrary[i].title}`;
+    bookAuthor.textContent = `Author: ${myLibrary[i].author}`;
+    bookPages.textContent = `Pages: ${myLibrary[i].pages}`;
+    readBtn.textContent = 'Read';
+    removeBtn.textContent = 'Remove from the library';
+    card.appendChild(bookTitle);
+    card.appendChild(bookAuthor);
+    card.appendChild(bookPages);
+    card.appendChild(readBtn);
+    card.appendChild(removeBtn);
+    library.appendChild(card);
+
+readBtn.addEventListener('click', () => {
+    if(readBtn.textContent = 'Read') {
+        card.style.backgroundColor = 'green';
+        readBtn.textContent = 'Not Read';
+    } else if(readBtn.textContent = 'Not Read') {
+        card.style.backgroundColor = 'red';
+        readBtn.textContent = 'Read';
     }
+})
+
+removeBtn.addEventListener('click', () => {
+    library.removeChild(card);
+})
+
 }
+
+
+addToLibraryBtn.addEventListener('click', function(e) {
+    e.preventDefault();
+    const title = document.getElementById('book-title').value;  
+    const author = document.getElementById('book-author').value;
+    const pages = Number(document.getElementById('book-pages').value);
+    const book = new Book(title, author, pages);
+    myLibrary.push(book);
+    addBookToLibrary();
+    i++;
+    
+})
 
